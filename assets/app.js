f651f9347c46aed8f74c573f7720e29c9b41e33b
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-07-20 14:58:39 -0700

    hybrid of some activities

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+  function renderButtons() {
+    $('#movies-view').empty();
+    for (var i = 0; i < movies.length; i++) {
+      var a = $('<button>');
+      a.addClass('movie');
+      a.attr('data-name', movies[i]);
+      a.text(movies[i]);
+      $('#moviePage').append(a);
+    }
+  }
+  $('#add-movie').on('click', function(event) {
+    event.preventDefault();
+    var movie = $('#movie-input')
+      .val()
+      .trim();
+    movies.push(movie);
+    renderButtons();
+  });
+});
